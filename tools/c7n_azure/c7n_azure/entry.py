a1fe652154d1ba899f978871a48bd2c0a0584230
Kirill Logachev <kiril@microsoft.com>
2018-09-14 18:08:48 -0700

    Add policy assignments resource

--- tools/c7n_azure/c7n_azure/entry.py
+++ tools/c7n_azure/c7n_azure/entry.py
@@ -41,6 +41,7 @@ import c7n_azure.resources.redis
 import c7n_azure.resources.vmss
 import c7n_azure.resources.batch
 import c7n_azure.resources.appserviceplan  # noqa: F401
+import c7n_azure.resources.policy_assignments
 
 
 def initialize_azure():
