e35888917fc87d3e5036aca076fea8d8ebf6c408
Kirill Logachev <kiril@microsoft.com>
2018-10-25 14:07:24 -0700

    Debug code

--- tools/c7n_mailer/c7n_mailer/cli.py
+++ tools/c7n_mailer/c7n_mailer/cli.py
@@ -190,8 +190,6 @@ def main():
     default_templates = [path.abspath(path.join(module_dir, 'msg-templates')),
                          path.abspath(path.join(module_dir, '..', 'msg-templates')),
                          path.abspath('.')]
-    print(default_templates)
-    exit(1)
     templates = args_dict.get('templates', None)
     if templates:
         default_templates.append(path.abspath(path.expanduser(path.expandvars(templates))))
