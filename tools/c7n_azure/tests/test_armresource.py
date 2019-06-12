0b85ef5012e8efacbc9e3bfccc4bbe2347f98e07
Kirill Logachev <logachev.k@gmail.com>
2019-06-12 12:11:25 -0700

    Update tests

--- tools/c7n_azure/tests/test_armresource.py
+++ tools/c7n_azure/tests/test_armresource.py
@@ -226,3 +226,43 @@ class ArmResourceTest(BaseTest):
         })
         p.run()
         delete_action_mock.assert_called_with([self.fake_arm_resources[0]])
+
+    def test_arm_resource_resource_type_schema_validate(self):
+        with self.sign_out_patch():
+            p = self.load_policy({
+                'name': 'test-azure-armresource-filter',
+                'resource': 'azure.armresource',
+                'filters': [
+                    {
+                        'type': 'resource-type',
+                        'values': ['Microsoft.Storage/storageAccounts', 'Microsoft.Web/serverFarms']
+                    }
+                ]
+            }, validate=True)
+            self.assertTrue(p)
+
+    @arm_template('vm.json')
+    def test_arm_resource_resource_type(self):
+        p = self.load_policy({
+            'name': 'test-azure-armresource-filter',
+            'resource': 'azure.armresource',
+            'filters': [
+                {
+                    'type': 'resource-type',
+                    'values': [
+                        'Microsoft.Network/virtualNetworks',
+                        'Microsoft.Storage/storageAccounts',
+                        'Microsoft.Compute/virtualMachines'
+                    ]
+                },
+                {
+                    'type': 'value',
+                    'key': 'resourceGroup',
+                    'value_type': 'normalize',
+                    'operation': 'eq',
+                    'value': 'test_vm'
+                }
+            ]
+        })
+        resources = p.run()
+        self.assertEqual(len(resources), 3)
