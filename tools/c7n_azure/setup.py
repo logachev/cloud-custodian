34a65f5cb57a460bc58909527fb09e1b66a3e1a7
Stefan Gordon <stefan@stefangordon.com>
2019-07-04 05:54:39 -0700

    lint

--- tools/c7n_azure/setup.py
+++ tools/c7n_azure/setup.py
@@ -73,7 +73,7 @@ setup(
                       "azure-keyvault",
                       "azure-storage-blob",
                       # azure-cosmosdb-table has incompatible dependency ~=1.1
-                      # Remove this when fixed: 
+                      # Remove this when fixed:
                       # https://github.com/Azure/azure-cosmos-table-python/issues/39
                       "azure-storage-common~=2.0",
                       "azure-storage-queue",
