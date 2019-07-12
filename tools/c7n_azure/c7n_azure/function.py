68f507f5fbb44971d02b94de6c311ff3b768e793
Kirill Logachev <logachev.k@gmail.com>
2019-07-11 17:31:50 -0700

    Minor refactor

--- tools/c7n_azure/c7n_azure/function.py
+++ tools/c7n_azure/c7n_azure/function.py
@@ -17,8 +17,10 @@ import sys
 
 from os.path import dirname, join
 
+function_directory = dirname(__file__)
+
 # The working path for the Azure Function doesn't include this file's folder
-sys.path.append(dirname(dirname(__file__)))
+sys.path.append(dirname(function_directory))
 
 from c7n_azure import handler, entry
 from c7n_azure.utils import ResourceIdParser
@@ -37,8 +39,8 @@ def main(input):
     logging.info("Running Azure Cloud Custodian Policy")
 
     context = {
-        'config_file': join(dirname(__file__), 'config.json'),
-        'auth_file': join(dirname(__file__), 'auth.json')
+        'config_file': join(function_directory, 'config.json'),
+        'auth_file': join(function_directory, 'auth.json')
     }
 
     event = None
