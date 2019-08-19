5c7251c00b8addf379109502396f6e752f9d7be4
Kirill Logachev <logachev.k@gmail.com>
2019-08-19 11:40:13 -0700

    Add test

--- tools/c7n_mailer/tests/test_schema.py
+++ tools/c7n_mailer/tests/test_schema.py
@@ -30,6 +30,8 @@ class MailerSchemaTest(unittest.TestCase):
         with self.assertRaises(exceptions.ValidationError):
             jsonschema.validate({'test': {'wrong': 'value'}},
                                 property_schema)
+            jsonschema.validate({'test': {'secret': 'https://secret_uri'}},
+                                property_schema)
             jsonschema.validate({'test': {'type': 'azure.keyvault',
                                           'secret': 'https://secret_uri', 'extra': 'e'}},
                                 property_schema)
