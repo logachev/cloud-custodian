e96068e40ccae71760fadbc367b9a7562b97707e
Kirill Logachev <logachev.k@gmail.com>
2019-08-27 14:56:19 -0700

    Add execute action

--- tools/c7n_azure/c7n_azure/resources/cost_management_export.py
+++ tools/c7n_azure/c7n_azure/resources/cost_management_export.py
@@ -12,13 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from c7n_azure.actions.base import AzureBaseAction
 from c7n_azure.provider import resources
 from c7n_azure.query import QueryResourceManager
+
+from c7n.utils import type_schema
 
 
 @resources.register('cost-management-export')
 class CostManagementExport(QueryResourceManager):
-    """ Cost Exports for current subscription
+    """ Cost Management Exports for current subscription (doesn't include Resource Group scopes)
 
     :example:
 
@@ -27,8 +30,8 @@ class CostManagementExport(QueryResourceManager):
     .. code-block:: yaml
 
         policies:
-          - name: get-cost-exports
-            resource: azure.cost-export
+          - name: get-cost--management-exports
+            resource: azure.cost-management-export
 
     """
 
@@ -50,3 +53,34 @@ class CostManagementExport(QueryResourceManager):
             scope = '/subscriptions/{0}'\
                 .format(resource_manager.get_session().get_subscription_id())
             return {'scope': scope}
+
+
+@CostManagementExport.action_registry.register('execute')
+class CostManagementExportActionExecute(AzureBaseAction):
+    """ Trigger Cost Management Export execution
+
+    Known issues:
+
+    Error: (400) A valid email claim is required. Email claim is missing in the request header.
+
+    :example:
+
+    Returns all cost exports for current subscription scope
+
+    .. code-block:: yaml
+
+        policies:
+          - name: get-cost--management-exports
+            resource: azure.cost-management-export
+            actions:
+              - type: execute
+
+    """
+    schema = type_schema('execute')
+
+    def _prepare_processing(self):
+        self.client = self.manager.get_client()
+        self.scope = 'subscriptions/{0}'.format(self.manager.get_session().get_subscription_id())
+
+    def _process_resource(self, resource):
+        self.client.exports.execute(self.scope, resource['name'])
