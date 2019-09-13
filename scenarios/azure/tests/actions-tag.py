0533d35f970aeb33d7eb77a43f5fcacb37a1ca16
Kirill Logachev <logachev.k@gmail.com>
2019-09-12 20:17:53 -0700

    v1

--- scenarios/azure/tests/actions-tag.py
+++ scenarios/azure/tests/actions-tag.py
@@ -1,14 +1,22 @@
 import unittest
 import pytest
-from parameterized import parameterized
+from parameterized import parameterized_class
+from python_terraform import *
 
 
 class BaseScenarioTest(unittest.TestCase):
 
+    t = Terraform()
+
     def setUp(self) -> None:
-        print('Deploying terraform template...')
+        print('Deploying terraform template...{0}'.format(self.template))
         self.resource_group = 'c7n-test-'
         self.resource_name = 'test'
+
+        return_code, stdout, stderr = self.t.apply('s:\\github\\cloud-custodian\\scenarios\\azure\\templates\\' + self.template)
+        print(return_code)
+        print(stdout)
+        print(stderr)
 
     def run_policy(self, policy, variables):
         print('run policy {0} {1}'.format(policy, variables))
@@ -18,26 +26,26 @@ class BaseScenarioTest(unittest.TestCase):
 
 
 scenarios = [
-    [{'resource': 'azure.resourcegroup',
-      'template': 'templates/resource-group/main.tf',
-      'policy': 'tag.yml'}],
-    [{'resource': 'azure.storage',
-      'template': 'templates/storage/main.tf',
-      'policy': 'tag.yml'}],
+    {'resource': 'azure.resourcegroup',
+     'template': 'resource-group',
+     'policy': 'tag.yml'},
+    # [{'resource': 'azure.storage',
+    #   'template': 'templates/storage/main.tf',
+    #   'policy': 'tag.yml'}],
     ]
 
 
+@parameterized_class(scenarios)
 class TestActionsTag(BaseScenarioTest):
 
-    @parameterized.expand(scenarios)
-    def test_execute_policy(self, scenario):
+    def test_execute_policy(self):
         variables = {
             'name': self.resource_name,
-            'resource': scenario['resource'],
+            'resource': self.resource,
             'tag-name': 'randomtag',
             'tag-value': 'randomvalue'
         }
-        self.run_policy(scenario['policy'], variables)
+        self.run_policy(self.policy, variables)
 
         print('Verifying policy...')
         self.assertFalse(True)
